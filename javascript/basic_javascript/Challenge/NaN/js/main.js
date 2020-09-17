2ec0cff77ccc7d766e1d1a12c5ac6c9a71968851
Michael Oh <60717081+LightspeedIcicle@users.noreply.github.com>
2020-09-17 08:59:30 -0700

    initial commit

---
+++ javascript/basic_javascript/Challenge/NaN/js/main.js
@@ -0,0 +1,5 @@
+function numitsnot() {
+    document.getElementById("notanum").innerHTML = 0 / 0;
+    document.getElementById("truenum").innerHTML = isNaN("a");
+    document.getElementById("falsenum").innerHTML = isNaN(3);
+}
